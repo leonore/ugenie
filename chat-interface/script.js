bbde37d4f11f30d0018612ad7962da4410331628
James <conwayjw97@gmail.com>
2018-11-29 22:39:40 +0000

    Optimized CSS

--- chat-interface/script.js
+++ chat-interface/script.js
@@ -4,7 +4,7 @@ const CLIENT_ID = '4cNswoNqM2wVFHPg';
 
 const drone = new ScaleDrone(CLIENT_ID, {
   data: { // Will be sent out as clientData via events
-    name: getRandomName(),
+    name: "You",
     color: getRandomColor(),
   },
 });
