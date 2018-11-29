6a2400a0023829c340038c32418cc5b3fe53fff3
2247492c <2247492c@student.gla.ac.uk>
2018-11-29 12:26:06 +0000

    Wrote the HTML for an embedded pop-up chatbox

--- chat-interface/script.js
+++ chat-interface/script.js
@@ -129,3 +129,11 @@ function addMessageToListDOM(text, member) {
     el.scrollTop = el.scrollHeight - el.clientHeight;
   }
 }
+
+function openForm() {
+    document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+}
