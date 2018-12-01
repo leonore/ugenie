74a69c4475ddc38e6330e1ff617f7a93e34032da
2247492c <2247492c@student.gla.ac.uk>
2018-12-01 13:36:56 +0000

    Added some more testing code to session.html

--- chat-interface/script.js
+++ chat-interface/script.js
@@ -1,138 +1,3 @@
-// PS! Replace this with your own channel ID
-// If you use this channel ID your app will stop working in the future
-const CLIENT_ID = '4cNswoNqM2wVFHPg';
-
-const drone = new ScaleDrone(CLIENT_ID, {
-  data: { // Will be sent out as clientData via events
-    name: "You",
-    color: getRandomColor(),
-  },
-});
-
-let members = [];
-
-drone.on('open', error => {
-  if (error) {
-    return console.error(error);
-  }
-  console.log('Successfully connected to Scaledrone');
-
-  const room = drone.subscribe('observable-room');
-  room.on('open', error => {
-    if (error) {
-      return console.error(error);
-    }
-    console.log('Successfully joined room');
-  });
-
-  room.on('members', m => {
-    members = m;
-    updateMembersDOM();
-  });
-
-  room.on('member_join', member => {
-    members.push(member);
-    updateMembersDOM();
-  });
-
-  room.on('member_leave', ({id}) => {
-    const index = members.findIndex(member => member.id === id);
-    members.splice(index, 1);
-    updateMembersDOM();
-  });
-
-  room.on('data', (text, member) => {
-    if (member) {
-      addMessageToListDOM(text, member);
-    } else {
-      // Message is from server
-    }
-  });
-});
-
-drone.on('close', event => {
-  console.log('Connection was closed', event);
-});
-
-drone.on('error', error => {
-  console.error(error);
-});
-
-function getRandomName() {
-  const adjs = ["autumn", "hidden", "bitter", "misty", "silent", "empty", "dry", "dark", "summer", "icy", "delicate", "quiet", "white", "cool", "spring", "winter", "patient", "twilight", "dawn", "crimson", "wispy", "weathered", "blue", "billowing", "broken", "cold", "damp", "falling", "frosty", "green", "long", "late", "lingering", "bold", "little", "morning", "muddy", "old", "red", "rough", "still", "small", "sparkling", "throbbing", "shy", "wandering", "withered", "wild", "black", "young", "holy", "solitary", "fragrant", "aged", "snowy", "proud", "floral", "restless", "divine", "polished", "ancient", "purple", "lively", "nameless"];
-  const nouns = ["waterfall", "river", "breeze", "moon", "rain", "wind", "sea", "morning", "snow", "lake", "sunset", "pine", "shadow", "leaf", "dawn", "glitter", "forest", "hill", "cloud", "meadow", "sun", "glade", "bird", "brook", "butterfly", "bush", "dew", "dust", "field", "fire", "flower", "firefly", "feather", "grass", "haze", "mountain", "night", "pond", "darkness", "snowflake", "silence", "sound", "sky", "shape", "surf", "thunder", "violet", "water", "wildflower", "wave", "water", "resonance", "sun", "wood", "dream", "cherry", "tree", "fog", "frost", "voice", "paper", "frog", "smoke", "star"];
-  return (
-    adjs[Math.floor(Math.random() * adjs.length)] +
-    "_" +
-    nouns[Math.floor(Math.random() * nouns.length)]
-  );
-}
-
-function getRandomColor() {
-  return '#' + Math.floor(Math.random() * 0xFFFFFF).toString(16);
-}
-
-//------------- DOM STUFF
-
-const DOM = {
-  membersCount: document.querySelector('.members-count'),
-  membersList: document.querySelector('.members-list'),
-  messages: document.querySelector('.messages'),
-  input: document.querySelector('.message-form__input'),
-  form: document.querySelector('.message-form'),
-};
-
-console.log("DOM.form:", DOM.form);
-
-DOM.form.addEventListener('submit', sendMessage);
-
-function sendMessage() {
-  const value = DOM.input.value;
-  if (value === '') {
-    return;
-  }
-  DOM.input.value = '';
-  drone.publish({
-    room: 'observable-room',
-    message: value,
-  });
-}
-
-function createMemberElement(member) {
-  const { name, color } = member.clientData;
-  const el = document.createElement('div');
-  el.appendChild(document.createTextNode(name));
-  el.className = 'member';
-  el.style.color = color;
-  return el;
-}
-
-function updateMembersDOM() {
-  // Commented out for now as members list isn't on the html
-  //DOM.membersCount.innerText = `${members.length} users in room:`;
-  //DOM.membersList.innerHTML = '';
-  //members.forEach(member =>
-  //  DOM.membersList.appendChild(createMemberElement(member))
-  //);
-}
-
-function createMessageElement(text, member) {
-  const el = document.createElement('div');
-  el.appendChild(createMemberElement(member));
-  el.appendChild(document.createTextNode(text));
-  el.className = 'message';
-  return el;
-}
-
-function addMessageToListDOM(text, member) {
-  const el = DOM.messages;
-  const wasTop = el.scrollTop === el.scrollHeight - el.clientHeight;
-  el.appendChild(createMessageElement(text, member));
-  if (wasTop) {
-    el.scrollTop = el.scrollHeight - el.clientHeight;
-  }
-}
-
 function openForm() {
     document.getElementById("myForm").style.display = "block";
     document.getElementsByClassName("open-button")[0].style.visibility = 'hidden';
@@ -141,4 +6,76 @@ function openForm() {
 function closeForm() {
     document.getElementById("myForm").style.display = "none";
     document.getElementsByClassName("open-button")[0].style.visibility = 'visible';
+}
+
+function Chat() {
+    this.update = updateChat; // Ask the server if there are new lines in the text file.
+    this.send = sendChat; // Called when a message is sent.
+    this.getState = getStateOfChat; // Ask the server how many lines the current text file is, so it has something to compare against and know when lines are "new" or not.
+}
+
+function getStateOfChat() {
+    if (!instance) {
+        instance = true;
+        $.ajax({
+            type: "POST",
+            url: "process.php",
+            data: {
+                'function': 'getState',
+                'file': file
+            },
+            dataType: "json",
+            success: function(data) {
+                state = data.state;
+                instance = false;
+            }
+        });
+    }
+}
+
+function updateChat() {
+    if (!instance) {
+        instance = true;
+        $.ajax({
+                type: "POST",
+                url: "process.php",
+                data: {
+                    'function': 'update',
+                    'state': state,
+                    'file': file
+                },
+                dataType: "json",
+                success: function(data) {
+                    if (data.text) {
+                        for (var i = 0; i < data.text.length; i++) {
+                            $('#chat-area').append($(""+ data.text[i] +""));
+                            }
+                        }
+                        document.getElementById('chat-area').scrollTop = document.getElementById('chat-area').scrollHeight;
+                        instance = false;
+                        state = data.state;
+                    }
+        });
+    }
+    else {
+        setTimeout(updateChat, 1500);
+    }
+}
+
+function sendChat(message, nickname) {
+    updateChat();
+    $.ajax({
+        type: "POST",
+        url: "process.php",
+        data: {
+            'function': 'send',
+            'message': message,
+            'nickname': nickname,
+            'file': file
+        },
+        dataType: "json",
+        success: function(data) {
+            updateChat();
+        }
+    });
 }
