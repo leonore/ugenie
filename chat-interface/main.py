4bd86958a21bb46b7adf726dfc2bf7bfdaa9a660
James <conwayjw97@gmail.com>
2018-12-01 15:42:20 +0000

    Flask chat working

--- chat-interface/main.py
+++ chat-interface/main.py
@@ -4,6 +4,7 @@ from flask_socketio import SocketIO
 app = Flask(__name__) # Wrap Flask around __name__
 app.config['SECRET_KEY'] = 'vnkdjnfjknfl1232#' # Secret key for encryption
 socketio = SocketIO(app) # Apply SocketIO to 'app' to use it instead of app for running the application 	
+
 # When the user enters the homepage ('/') it triggers the sessions view
 @app.route('/')
 def sessions():
@@ -15,7 +16,8 @@ def messageReceived(methods=['GET', 'POST']):
 @socketio.on('my event')
 def handle_my_custom_event(json, methods=['GET', 'POST']):
 	print('Received event: ' + str(json))
-	socketio.emit('My response', json, callback=messageReceived)
+	# If the message is received by the server messageReceived is called
+	socketio.emit('my response', json, callback=messageReceived)
 
 if __name__ == '__main__':
 	# Takes optional host and port arguments but by efault will listen on localhost:5000
