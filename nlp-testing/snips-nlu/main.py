a998d6cdab7abfee8c113dd198944ee3ca49ae2a
Leonore Papaloizos <2264897V@student.gla.ac.uk>
2018-12-13 15:07:52 +0000

    testing the rasa stack, see RASA.md for steps

--- nlp-testing/snips-nlu/main.py
+++ nlp-testing/snips-nlu/main.py
@@ -5,12 +5,12 @@ import snips_nlu
 from snips_nlu import SnipsNLUEngine
 from snips_nlu.default_configs import CONFIG_EN
 
-with io.open("sample_datasets/flights_dataset.json") as f:
+with io.open("sample_datasets/test_dataset.json") as f:
     sample_dataset = json.load(f)
 
 snips_nlu.load_resources('snips_nlu_en')
 nlu_engine = SnipsNLUEngine(config=CONFIG_EN)
 nlu_engine = nlu_engine.fit(sample_dataset)
-text = "Book me a flight to go to boston this weekend"
+text = "you understood me"
 parsing = nlu_engine.parse(text)
-print(parsing["intent"]["intentName"])
+print(parsing)
