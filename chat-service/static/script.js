7e3ed78146542532651f4273c80040a8a68ade50
James <conwayjw97@gmail.com>
2019-02-13 15:28:15 +0000

    Cleaned and commented script.js

--- chat-service/static/script.js
+++ chat-service/static/script.js
@@ -11,23 +11,29 @@ function closeForm() {
 // Establish the connection and create the session
 // document.domain represents the IP address of the computer you are working on and location.port represents the port
 var socket = io.connect('http://' + document.domain + ':' + location.port);
+
+// When the user connects, run this function
 socket.on('connect', function() {
 	console.log('New connection from ' + document.domain);
+	
+	// Sent an event to the server with details on the newly connected user
     socket.emit('new_connection', {
         state: 'User Connected',
 		ip: document.domain
     });
+	
+	// When the user submits a new message, send a message event to the server
     var form = $('form').on('submit', function(e) {
         e.preventDefault()
-        let user_name = 'You'
-        let user_input = $('input.message-form__input').val()
         socket.emit('new_message', {
-			user_name: user_name,
-            message: user_input
+			user_name: 'You',
+            message: $('input.message-form__input').val()
         })
         $('input.message-form__input').val('').focus()
     });
 })
+
+// When the client receives a 'print message' event, print the message on the chat
 socket.on('print message', function(msg) {
     console.log(msg);
     if (typeof msg.user_name !== 'undefined') {
