ac1c5bfb2871cd9083c422d620a1ca9f5b06d2b5
2229683a <2229683a@student.gla.ac.uk>
2019-02-26 10:47:37 +0000

    including gitlab-ci.yaml + cleaning the bbd-test

--- bbd/tests/features/steps/steps.py
+++ bbd/tests/features/steps/steps.py
@@ -14,40 +14,47 @@ def one_chatbot(context):
       assert(chatbot_open_assert(context) ==True)
 
 
+
 @when(u'the user open the chat')
 def step_impl(context):
     try:
-        context.driver.find_element_by_class_name("open-button").click();
-        context.driver.implicitly_wait(1)
-
+        context.driver=open_chatbot(context.driver)
     except:
-        raise Exception("NOO ")
+        raise Exception("chatbot cannot be opened ,either because it is already open or the open button is not working ")
+
+
 
 @when(u'the user ask question about a specific subject')
 def step_impl(context):
     try :
-        context.driver.find_element_by_xpath('//*[@id="myForm"]/div/form/input').click()
-        context.driver.find_element_by_xpath('//*[@id="myForm"]/div/form/input').send_keys("what is biology")
-        context.driver.find_element_by_xpath('//*[@id="myForm"]/div/form/input').submit()
-        context.chatbot_message_num += 2
-        context.driver.implicitly_wait(1)
+        context.driver,context.chatbot_message_num =write_into_felid(context.driver,context.chatbot_message_num,"what is biology")
 
     except:
-        raise Exception("cannot change driver values")
+        raise Exception("cannot send a message as a user ")
+
+
+
 
 @then(u'the chatbot should search for the information in the database')
 def step_impl(context):
     """code implemented to the query"""
     pass
 
+
+
+
 @when(u'it dose not fine the information required')
 def step_impl(context):
-   """code iomplemented  for local virision"""
+   """code implemented  for local version"""
    pass
+
+
 
 @then(u'it should send that a message that  it dose not understand the question')
 def step_impl(context):
     pass
+
+
 
 
 @then(u'the chatbot should search the database It found the answer')
@@ -57,10 +64,13 @@ def step_impl(context):
 
 @then(u'should send it back to the user')
 def step_impl(context):
-    context.chatbot_message = context.driver.find_element_by_xpath(
-        ('//*[@id="myForm"]/div/div[2]/div[{}]').format(context.chatbot_message_num))
+    context.chatbot_message = chatbot_xpath(context.driver,context.chatbot_message_num)
     print(context.chatbot_message.text)
     assert ('Ecology & Environmental Biology is a Life Sciences PGT course' == context.chatbot_message.text)
+
+
+
+
 
 @given(u'the user in the specific page')
 def step_impl(context):
@@ -68,14 +78,15 @@ def step_impl(context):
 
 
 
+
 @then(u'the widget expands and the chatbot starts a conversation')
 def step_impl(context):
     assert (chatbot_open_assert(context) == False)
     try:
-     context.chatbot_message =context.driver.find_element_by_xpath(('//*[@id="myForm"]/div/div[2]/div[{}]').format(context.chatbot_message_num))
+     context.chatbot_message = chatbot_xpath(context.driver,context.chatbot_message_num)
      print(context.chatbot_message.text)
     except:
-        raise Exception("chatbot message did not reach")
+        raise Exception("the chatbot message cannot be seen  ")
 
     assert(context.chatbot_message.text=="Hello, I'm GUVA, the Glasgow University Virtual Assistant. How can I help you?")
 
@@ -104,21 +115,24 @@ def setp_impl(context):
 @when(u'the user asks for further')
 def step_impl(context):
     context.driver, context.chatbot_message_num = write_into_felid(context.driver, context.chatbot_message_num,"how much is it ")
-    context.driver.implicitly_wait(1)
-    context.user_message = context.driver.find_element_by_xpath(
-        chatbot_message_xpath(4))
+
+    context.user_message =  chatbot_xpath(context.driver,4)
     print(context.user_message.text)
+
+
+
 
 
 @then(u'the chatbot should send the requierd information if it dose exist')
 def step_impl(context):
-    context.chatbot_message = context.driver.find_element_by_xpath(
-        ('//*[@id="myForm"]/div/div[2]/div[{}]').format(context.chatbot_message_num))
+    context.chatbot_message = chatbot_xpath(context.driver,context.chatbot_message_num)
     print(context.chatbot_message.text)
     assert ("Did you want the course: Ecology & Environmental Biology? (yes/no)" == context.chatbot_message.text)
 
 
+
+
+
 @when(u'chatbot is accessible')
 def step_impl(context):
-    context.driver.find_element_by_class_name("open-button").click();
-    context.driver.implicitly_wait(1)
+    context.driver=open_chatbot(context.driver)
