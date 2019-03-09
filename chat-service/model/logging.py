2f0d4b653ff0de2794cdf6896aa85ff5d9b5b86e
James <conwayjw97@gmail.com>
2019-03-09 18:10:27 +0000

    logging.py cleanup

--- chat-service/model/logging.py
+++ chat-service/model/logging.py
@@ -2,10 +2,10 @@ import datetime
 
 def logUser(sessionId, message):
     now = datetime.datetime.now()
-    with open(str(sessionId)+".txt", "w") as out:
-        out.write(now.strftime("%Y-%m-%d %H:%M") + " User: " + str(message))
+    with open('chat-logs/' + str(sessionId) + '.txt', 'w') as out:
+        out.write(now.strftime('%y-%m-%d %H:%M') + ' User: ' + str(message) + '/r/n')
 
 def logAgent(sessionId, message):
     now = datetime.datetime.now()
-    with open(str(sessionId)+".txt", "w") as out:
-        out.write(now.strftime("%Y-%m-%d %H:%M") + " ChatBot: " + str(message))
+    with open('chat-logs/' + str(sessionId) + '.txt', 'w') as out:
+        out.write(now.strftime('%y-%m-%d %H:%M') + ' ChatBot: ' + str(message) + '/r/n')
