9470a39eb066d3e93b10096ea8ca981a4a443e3b
2229683a <2229683a@student.gla.ac.uk>
2019-03-10 00:02:45 +0000

    Fixes #144 ,fixing the Scenario: the remember  the user questions_1

--- tests/bdd/tests/features/steps/steps.py
+++ tests/bdd/tests/features/steps/steps.py
@@ -28,13 +28,13 @@ def step_impl(context):
         if(elem.is_displayed()==False):
             onne_button =context.driver.find_element_by_class_name("open-button")
             onne_button.click()
-            context.driver.implicitly_wait(1)
+            context.driver.implicitly_wait(2)
         elem.click()
         elem.clear()
         elem.send_keys("what is biology")
         elem.submit()
         context.chatbot_message_num += 2
-        context.driver.implicitly_wait(1)
+        context.driver.implicitly_wait(2)
         context.user_message= chatbot_xpath(context.driver, 2)
 
         print(context.user_message.text)
@@ -129,13 +129,13 @@ def step_impl(context):
     if (elem.is_displayed() == False):
         onne_button = context.driver.find_element_by_class_name("open-button")
         onne_button.click()
-        context.driver.implicitly_wait(1)
+        context.driver.implicitly_wait(2)
     elem.click()
     elem.clear()
     elem.send_keys("how much is it")
     elem.submit()
     context.chatbot_message_num  +=2
-    context.driver.implicitly_wait(1)
+    context.driver.implicitly_wait(2)
     context.user_message =  chatbot_xpath(context.driver,4)
     print(context.user_message.text)
 
@@ -165,4 +165,4 @@ def step_impl(context):
     if (elem.is_displayed() == False):
         onne_button = context.driver.find_element_by_class_name("open-button")
         onne_button.click()
-        context.driver.implicitly_wait(1)
+        context.driver.implicitly_wait(2)
