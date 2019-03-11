8fb8a5bee5b12febcd1146d147e518e87b1d04ba
Leonore Papaloizos <2264897V@student.gla.ac.uk>
2019-03-11 13:32:00 +0000

    added course type check in PT/FT action for #147

--- chat-service/model/actions.py
+++ chat-service/model/actions.py
@@ -93,11 +93,15 @@ class PTorFTCheck(Action):
 
     def run(self, dispatcher, tracker, domain):
         elastic_title, elastic_cat, elastic_score = elastic.get_course_title(tracker.get_slot("course"))
-        pt_ft_answer = elastic.check_pt_ft_course(elastic_title)
+        if elastic_cat == "AD":
+            pt_ft_answer = elastic.check_pt_ft_course(elastic_title)
+        else:
+            pt_ft_answer = "Sorry, I can only check for part-time/full-time for PGT courses."
 
         dispatcher.utter_message(pt_ft_answer)
-        return [SlotSet("course_type", "admissions")]
-
+        # removing this as it clashes with the "else" answer
+        #return [SlotSet("course_type", "admissions")]
+        return
 
 # Utters time related information to do with a course
 # e.g. start time, year
