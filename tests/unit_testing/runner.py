e066b05108d4b45ff372b8a442aec93e9c6ce3e2
Leonore Papaloizos <2264897V@student.gla.ac.uk>
2019-03-15 16:15:37 +0000

    good practices cleanup, typo changes, yaml formatting fix

--- tests/unit_testing/runner.py
+++ tests/unit_testing/runner.py
@@ -1,23 +1,16 @@
-# testsrunner.py
 import unittest
 import sys
-#elastic path
-#path should be changed
-sys.path.insert(0, '/home/ubuntu/dissertation/tests/unit_testing')
-# import your test modules
+
+sys.path.append('chat-service/model')
 import test_elastic
-
-
 
 # initialize the test suite
 loader = unittest.TestLoader()
 suite  = unittest.TestSuite()
 
 # add tests to the test suite
-###suite.addTests(loader.loadTestsFromModule(soketio_test))
 suite.addTests(loader.loadTestsFromModule(test_elastic))
 
-
-# initialize a runner, pass it your suite and run it
+# initialize a runner, pass it to the suite and run it
 runner = unittest.TextTestRunner(verbosity=2)
 result = runner.run(suite)
