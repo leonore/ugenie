3a3475bff47ca020b849aaf4b3f6692ede68df58
2229683a <2229683a@student.gla.ac.uk>
2019-03-15 22:07:30 +0000

    Fixes #169 , unit test chatbot srvices

--- tests/unit_testing/runner.py
+++ tests/unit_testing/runner.py
@@ -2,15 +2,16 @@ import unittest
 import sys
 
 sys.path.append('../../chat-service/model')
+sys.path.append('../../chat-service/')
 import test_elastic
-
+import test_main
 # initialize the test suite
 loader = unittest.TestLoader()
 suite  = unittest.TestSuite()
 
 # add tests to the test suite
 suite.addTests(loader.loadTestsFromModule(test_elastic))
-
+suite.addTests(loader.loadTestsFromModule(test_main))
 # initialize a runner, pass it to the suite and run it
 runner = unittest.TextTestRunner(verbosity=2)
 result = runner.run(suite)
---
+++ tests/unit_testing/test_main.py
@@ -0,0 +1,36 @@
+import sys
+#elastic path
+#path should be changed
+sys.path.insert(0, '/dissertation/chat-service')
+import unittest
+import main
+
+from flask import Flask, session, request, json as flask_json
+from flask_socketio import SocketIO, send, emit
+
+
+class TestSocketIO(unittest.TestCase):
+
+    # testing a connection to the rasa agent
+    def test_handle_connection(self):
+        client = main.socketio.test_client(main.app)
+        client.emit('user_joined','json')
+        received = client.get_received()
+        print(received)
+        expected_data = {"Hello, I'm UGenie,"}
+        self.assertEqual("Hello, I'm UGenie," in received[0]['args'][0]['message'], True)
+        self.assertEqual(len(received), 1)
+
+    # testing sending message to the rasa agent
+    def test_new_message(self):
+        client = main.socketio.test_client(main.app)
+        client.emit('new_message', {'user_name' : 'you','message':"hi"})
+        received = client.get_received()
+        self.assertEqual(len(received), 2)
+        print(received[0]['args'])
+        print(received[1]['args'])
+        self.assertEqual('Hi' in received[1]['args'][0]['message'] or 'Hello' in received[1]['args'][0]['message'],True)
+
+
+if __name__ == '__main__':
+    unittest.main()
